91ae44a14958a0e98466f6f66cb0796920cb3fdd
sean.mcgarrity <sean@tinpotteam.com>
2022-03-02 23:42:58 +0000

    Feat: Add Example flight controls

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,84 @@
 import * as Phaser from "phaser";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
-  active: false,
-  visible: false,
-  key: "Game",
+    active: false,
+    visible: false,
+    key: "Game",
 };
 
 export class GameScene extends Phaser.Scene {
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-  private background!: Phaser.GameObjects.Image;
+    private speed = 300;
+    private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    private emitter: Phaser.GameObjects.Particles.ParticleEmitter;
+    private cursors;
 
-  private player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    constructor() {
+        super(sceneConfig);
+    }
 
-  constructor() {
-    super(sceneConfig);
-  }
+    public preload() {
+        this.load.image("orc-man", "/assets/orangeship3.png");
+        this.load.image("spark", "/assets/blue.png");
+    }
 
-  public preload() {
-    this.load.image("player", "../assets/green_ship.png");
-    this.load.image("background", "../assets/blue-background.jpg");
-  }
+    public create() {
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-  public create() {
-    this.cursors = this.input.keyboard.createCursorKeys();
+        this.player = this.physics.add.sprite(20, 30, "orc-man");
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.player.setDrag(100);
+        this.player.setMaxVelocity(900);
+        this.emitter = this.add.particles("spark").createEmitter({
+            angle: { min: 180, max: 360 },
+            speed: 200,
+            lifespan: 400,
+            quantity: 8,
+            scale: { start: 0.2, end: 0.0 },
 
-    this.background = this.add.image(0, 0, "background");
-    this.background.setOrigin(0, 0);
-    this.background.setAlpha(1);
-    this.background.setBlendMode(Phaser.BlendModes.DARKEN);
-    this.background.displayWidth = window.innerWidth;
-    this.background.displayHeight = window.innerHeight;
+            blendMode: "ADD",
+        });
+    }
 
-    this.player = this.physics.add.sprite(500, 550, "player", 1);
-    this.player.setOrigin(0.5, 0.5);
-    this.player.setScale(0.5);
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds();
-    this.player.body.drag.setTo(400, 400);
-  }
+    public update() {
+        if (this.cursors.left.isDown) {
+            this.player.setAngularVelocity(-300);
+        } else if (this.cursors.right.isDown) {
+            this.player.setAngularVelocity(300);
+        } else {
+            this.player.setAngularVelocity(0);
+        }
 
-  private forwardThrust: number = 0;
-
-  update() {
-    
-  }
+        if (this.cursors.up.isDown) {
+            this.physics.velocityFromRotation(
+                this.player.rotation,
+                200,
+                this.player.body.acceleration
+            );
+        } else {
+            this.player.setAcceleration(0);
+        }
+        this.emitter.setPosition(this.player.x, this.player.y);
+        this.emitter.setAngle(this.player.rotation);
+    }
 }
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
-  title: "Sample",
-  type: Phaser.AUTO,
-  scene: GameScene,
-  scale: {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  },
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: true,
+    title: "Sample",
+    type: Phaser.AUTO,
+    scene: GameScene,
+    scale: {
+        width: window.innerWidth,
+        height: window.innerHeight,
     },
-  },
-  parent: "game",
-  backgroundColor: "#0000ff",
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true,
+        },
+    },
+    parent: "game",
+    backgroundColor: "#667766",
 };
 
 export const game = new Phaser.Game(gameConfig);
