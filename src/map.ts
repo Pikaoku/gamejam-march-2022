7571f31cdbb5c2790d11aa8793b196cf9cc51489
Joebias <joemarley1@gmail.com>
2022-03-05 22:17:53 +0000

    clean orbit code

--- src/map.ts
+++ src/map.ts
@@ -12,6 +12,7 @@ class Planet extends Phaser.Physics.Arcade.Sprite {
 	private speed: number;
 	private pathSpeed: number;
 	private pathVector: Phaser.Math.Vector2;
+	private sprite;
 
 	constructor(scene, x, y, width, height, speed) {
 		console.log("Test2");
@@ -26,19 +27,6 @@ class Planet extends Phaser.Physics.Arcade.Sprite {
 		this.setPosition(this.pathVector.x, this.pathVector.y);
 		console.log("Test1");
 	}
-
-	create(scene, x, y, width, height) {
-		console.log("Test1");
-		this.path = new Phaser.Curves.Ellipse(x, y, width, height);
-		this.pathIndex = 0;
-		this.pathSpeed = this.speed;
-		this.pathVector = new Phaser.Math.Vector2();
-
-		this.path.getPoint(0, this.pathVector);
-
-		this.setPosition(this.pathVector.x, this.pathVector.y);
-	}
-
 	preUpdate(time, delta) {
 		console.log("Test1");
 		this.anims.update(time, delta);
@@ -59,7 +47,7 @@ export class MapScene extends Phaser.Scene {
 	public preload() {
 		this.load.image("space-background", "/assets/space-background.jpg");
 		this.load.image("sun", "/assets/sun.png");
-		this.load.image("planet", "/assets/planet2.jpg");
+		this.load.image("planet", "/assets/first-rock-from-the-sun.png");
 	}
 
 	public create() {
@@ -82,8 +70,14 @@ export class MapScene extends Phaser.Scene {
 			.setOrigin(0.5);
 
 		const planets = this.physics.add.group({ allowGravity: false });
-		planets.add(new Planet(this, 150, 100, 1000, 1000, 0.005), true);
-		planets.add(new Planet(this, 150, 800, 1000, 1000, 0.005), true);
+		planets.add(
+			new Planet(this, screenCenterX, screenCenterY, 700, 700, 0.0005),
+			true
+		);
+		planets.add(
+			new Planet(this, screenCenterX, screenCenterY, 350, 350, 0.0005),
+			true
+		);
 	}
 }
 
