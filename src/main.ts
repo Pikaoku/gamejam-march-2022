c7f7a089b5a4e1aceb47c18802c64866c3218322
Joebias <joemarley1@gmail.com>
2022-10-17 13:05:55 +0100

    Ship added to main scene

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 export class GameScene extends Phaser.Scene {
     private speed = 300;
-    private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    private ship: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     private emitter: Phaser.GameObjects.Particles.ParticleEmitter;
     private cursors;
 
@@ -25,11 +25,11 @@ export class GameScene extends Phaser.Scene {
     public create() {
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.player = this.physics.add.sprite(20, 30, "orc-man");
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-        this.player.setDrag(100);
-        this.player.setMaxVelocity(900);
+        this.ship = this.physics.add.sprite(500 ,500, "orc-man");
+        this.ship.setBounce(0.2);
+        this.ship.setCollideWorldBounds(true);
+        this.ship.setDrag(100);
+        this.ship.setMaxVelocity(900);
 
         const asteroid = this.physics.add.sprite(200, 300, "asteroid");
         asteroid.setScale(0.2);
@@ -38,8 +38,8 @@ export class GameScene extends Phaser.Scene {
         asteroid.setBounce(0.6);
         asteroid.setCollideWorldBounds(true);
 
-        this.player.setMaxVelocity(1800);
-        this.physics.add.collider(this.player, asteroid);
+        this.ship.setMaxVelocity(1800);
+        this.physics.add.collider(this.ship, asteroid);
 
         this.emitter = this.add.particles("spark").createEmitter({
             angle: { min: 180, max: 360 },
@@ -54,24 +54,24 @@ export class GameScene extends Phaser.Scene {
 
     public update() {
         if (this.cursors.left.isDown) {
-            this.player.setAngularVelocity(-300);
+            this.ship.setAngularVelocity(-300);
         } else if (this.cursors.right.isDown) {
-            this.player.setAngularVelocity(300);
+            this.ship.setAngularVelocity(300);
         } else {
-            this.player.setAngularVelocity(0);
+            this.ship.setAngularVelocity(0);
         }
 
         if (this.cursors.up.isDown) {
             this.physics.velocityFromRotation(
-                this.player.rotation,
+                this.ship.rotation,
                 200,
-                this.player.body.acceleration
+                this.ship.body.acceleration
             );
         } else {
-            this.player.setAcceleration(0);
+            this.ship.setAcceleration(0);
         }
-        this.emitter.setPosition(this.player.x, this.player.y);
-        this.emitter.setAngle(this.player.rotation);
+        this.emitter.setPosition(this.ship.x, this.ship.y);
+        this.emitter.setAngle(this.ship.rotation);
     }
 }
 
